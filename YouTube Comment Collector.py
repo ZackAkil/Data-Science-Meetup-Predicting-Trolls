446e34198a145f96d064a8fae267176bc49a6e2e
ZackAkil <zackakil94@gmail.com>
2016-08-23 19:20:17 +0100

    added comment to csv with replies count

--- YouTube Comment Collector.py
+++ YouTube Comment Collector.py
@@ -8,6 +8,7 @@ import httplib2
 import os
 import sys
 import csv
+import copy
 
 keyFile = open('key.txt', 'r')
 apiKey = keyFile.readline()
@@ -20,7 +21,10 @@ def getComments(id,maxResults):
     data = json.loads(response.read())
 
     for c in data['items']:
-        comment = c['snippet']['topLevelComment']['snippet']['textDisplay'].encode('utf-8')
+        comment = {
+        'text':c['snippet']['topLevelComment']['snippet']['textDisplay'].encode('utf-8'),
+        'replies':c['snippet']['totalReplyCount']
+        }
         # print(comment)
         comments.append(comment)
     return comments
@@ -38,21 +42,29 @@ def getPopularVideos(maxResults):
                 'title': c['snippet']['title'].encode('utf-8'),
                 'likes': c['statistics']['likeCount'].encode('utf-8'),
                 'dislikes': c['statistics']['dislikeCount'].encode('utf-8'),
-                'favourites': c['statistics']['favoriteCount'].encode('utf-8'),
+                # 'favourites': c['statistics']['favoriteCount'].encode('utf-8'),
                 'views': c['statistics']['viewCount'].encode('utf-8'),
-                'comments': c['statistics']['commentCount'].encode('utf-8')
+                'commentCount': c['statistics']['commentCount'].encode('utf-8')
                 })
     return vids
 
-vids = getPopularVideos(5)
+vids = getPopularVideos(50)
 
 # for i in ids:
 #     getComments(i,100)
+commentData = []
 
 for v in vids:
     print(v['title'])
+    coms = getComments(v['id'],100)
+    for c in coms:
+        record = copy.deepcopy(v)
+        record['comment'] = c['text']
+        record['replies'] = c['replies']
+        commentData.append(record)
+        print(record)
 
-keys = vids[0].keys()
+keys = commentData[0].keys()
 
 with open('utube.csv', 'w') as csvfile:
 
@@ -60,7 +72,7 @@ with open('utube.csv', 'w') as csvfile:
 
     writer.writeheader()
 
-    for v in vids:
-        writer.writerow(v)
+    for c in commentData:
+        writer.writerow(c)
     # writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
     # writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
