4aacb4329f5386a28e9f441b84f21612659f9726
ZackAkil <zackakil94@gmail.com>
2016-08-29 00:08:29 +0100

    added to primetext library and text script

--- PrimeText.py
+++ PrimeText.py
@@ -1,37 +1,50 @@
 import numpy as np
+from nltk.stem.lancaster import LancasterStemmer
+from autocorrect import spell
 
 class PrimeText:
 
-	indexedDictionary = dict()
-	indexedRecords = []
+    indexedDictionary = dict()
+    indexedRecords = []
+ 
+    def hw(self):
+        print('sup')
+        
+    def assembleDictionary(self, textList):
+        st = LancasterStemmer()
+        uniqueWords = []
+        for text in textList:
+            for word in text.split(' '):
+                if st.stem(spell(word)) not in uniqueWords:
+                    uniqueWords.append(word)
+        return uniqueWords
 
-	 
-
-	def indexDictionary(dictionary):
-		primes = np.genfromtxt ('primes.csv', delimiter=",").astype(int)
-		fitPrime = primes[1:len(dictionary)+1,1]
-		indexedDictionary = dict(np.c_[dictionary,primeFit])
-
-	def indexComments(comments):
-		for comment in comments:
-			prod = 1
-			words = comment.split(' ')
-			for word in words:
-				if word in indexedDictionary:
-					prod *= int(indexedDictionary[word])
-			output.append(prod)
-		indexedRecords = output
-
-	def convertWordsToProduct(words):
-		output = 1
-		for word in words:
-			if word in indexedDictionary:
-				output *= int(indexedDictionary[word])
-		return output
-
-	def searchByPrimeFact(searchProduct):
-		return  (np.mod(indexedRecords, searchProduct) == 0)
-
-	def find(words):
-		prod = convertWordsToProduct(words)
-		return searchByPrimeFact(prod)
+#	def indexDictionary(self,dictionary):
+#         primes = np.genfromtxt ('primes.csv', delimiter=",").astype(int)
+#         fitPrime = primes[1:len(dictionary)+1,1]
+#         self.indexedDictionary = dict(np.c_[dictionary,fitPrime])
+#
+#	def indexComments(self,comments):
+#         output = []
+#         for comment in comments:
+#             prod = 1
+#             words = comment.split(' ')
+#             for word in words:
+#                 if word in self.indexedDictionary:
+#                     prod *= int(self.indexedDictionary[word])
+#                 output.append(prod)
+#        self.indexedRecords = output
+#
+#	def convertWordsToProduct(self,words):
+#		output = 1
+#		for word in words:
+#			if word in self.indexedDictionary:
+#				output *= int(self.indexedDictionary[word])
+#		return output
+#
+#	def searchByPrimeFact(self,searchProduct):
+#		return  (np.mod(self.indexedRecords, searchProduct) == 0)
+#
+#	def find(self, words):
+#		prod = convertWordsToProduct(words)
+#		return searchByPrimeFact(prod)
---
+++ PrimeTextTest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 28 21:29:38 2016
+
+@author: zackakil
+"""
+
+from PrimeText import PrimeText
+
+pt = PrimeText()
